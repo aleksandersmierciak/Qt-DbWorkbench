53236aea14f5c1794a14b8c7a9b74745cb5d97dd
m4gik <michal.szczygiel@wp.pl>
2014-01-20 21:14:06 +0100

    Improve generating class

--- MariaDB/MariaDB Java WorkBench/databaseinsertion/src/main/java/com/m4gik/util/Profile.java
+++ MariaDB/MariaDB Java WorkBench/databaseinsertion/src/main/java/com/m4gik/util/Profile.java
@@ -1,15 +1,24 @@
 package com.m4gik.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Random;
+import java.util.logging.Logger;
+
+import com.m4gik.database.MariaDBConnection;
 
 /**
  * This class is represents table inside database and is responsible for
- * generating SQL querys, and inserts data to database.
+ * generating SQL query, and inserts data to database.
  * 
  * @author Michał Szczygieł <michal.szczygiel@wp.pl>
  * 
@@ -31,6 +40,17 @@ public class Profile {
     private enum ProfileRelationShip {
         Engaged, InAnOpenRelationShip, InARelationhip, IsSingle, ItsComplicated, Married, Unspecified
     }
+
+    /**
+     * Logger for event registration.
+     */
+    private final static Logger logger = Logger.getLogger(Profile.class
+            .getName());
+
+    /**
+     * Query to be executed on database
+     */
+    private static final String MAX_USER_ID = "SELECT MAX(UserId) FROM `mszczygiel_ZTBD`.`Profile`";
 
     private String aboutMe;
 
@@ -60,11 +80,13 @@ public class Profile {
 
     private String lastname;
 
+    private Integer maxInserts;
+
     private String pictureURL;
 
     private String politicanViews;
 
-    private String query = "INSERT INTO `ZTBD`.`FacebookProfile` (`AboutMe`, "
+    private String query = "INSERT INTO `mszczygiel_ZTBD`.`Profile` (`AboutMe`, "
             + "`Activities`,`Birthday`,`FavoriteBooks`, `FavoriteMovies`,`FavoriteMusic`,"
             + "`FavoriteQuotes`,`FavoriteTVShows`, `Firstname`,`Lastname`,`Interests`,"
             + "`PictureURL`,`PoliticalViews`,`Religion`, `SignificantOtherId`,`UpdateTime`,`UserId`,"
@@ -78,7 +100,7 @@ public class Profile {
 
     private Integer significantOtherId;
 
-    private Timestamp updateTime;
+    private Time updateTime;
 
     /**
      * Primary key.
@@ -86,17 +108,63 @@ public class Profile {
     private Integer userId;
 
     /**
+     * The constructor for {@link Profile}.
+     * 
+     * @param maxInserts
+     *            Parameter describe max amount of inserts, which will be put
+     *            into database.
+     * @throws SQLException
+     */
+    public Profile(Integer maxInserts) throws SQLException {
+        this.userId = findFreeId();
+        logger.info("First free id: " + userId.toString());
+        setMaxInserts(maxInserts);
+    }
+
+    /**
      * This method prepares sql statement to insert auto-generated data.
      * 
      * @param conn
      * @return The prepared statement.
+     * @throws UnsupportedEncodingException
+     * @throws NoSuchAlgorithmException
      */
-    public PreparedStatement createInsert(Connection conn) {
+    public PreparedStatement createInsert(Connection conn)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
         PreparedStatement preparedStatement = null;
 
         try {
             preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setString(1, null);
+            preparedStatement.setString(1, getAboutMe());
+            preparedStatement.setString(2, getActivities());
+            preparedStatement.setDate(3, getBirthday());
+            preparedStatement.setString(4, getFavouriteBooks());
+            preparedStatement.setString(5, getFavouriteMovies());
+            preparedStatement.setString(6, getFavouriteMusic());
+            preparedStatement.setString(7, getFavouriteQuotes());
+            preparedStatement.setString(8, getFavouriteTVShows());
+            preparedStatement.setString(9, getFirstname());
+            preparedStatement.setString(10, getLastname());
+            preparedStatement.setString(11, getInterests());
+            preparedStatement.setString(12, getPictureURL());
+            preparedStatement.setString(13, getPoliticanViews());
+            preparedStatement.setString(14, getReligion());
+            preparedStatement.setInt(15, getSignificantOtherId());
+            preparedStatement.setTime(16, getUpdateTime());
+            preparedStatement.setInt(17, getUserId());
+            preparedStatement.setString(18,
+                    new RandomEnum<Gender>(Gender.class).random().toString());
+            preparedStatement.setString(19, new RandomEnum<LookingForGenders>(
+                    LookingForGenders.class).random().toString());
+            preparedStatement.setString(20, new RandomEnum<ProfileLookingFor>(
+                    ProfileLookingFor.class).random().toString());
+            preparedStatement.setString(21,
+                    new RandomEnum<ProfileRelationShip>(
+                            ProfileRelationShip.class).random().toString());
+            preparedStatement.setInt(22, 1);
+            preparedStatement.setInt(23, 1);
+            preparedStatement.setInt(24, 1);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -105,9 +173,22 @@ public class Profile {
     }
 
     /**
+     * This method finds first free id.
+     * 
+     * @return
+     * @throws SQLException
+     */
+    private Integer findFreeId() throws SQLException {
+        ResultSet result = MariaDBConnection.executeQuery(MAX_USER_ID);
+        result.next();
+        return result.getInt(1) + 1;
+    }
+
+    /**
      * @return the aboutMe
      */
     public String getAboutMe() {
+        aboutMe = Container.abouts[getRandom(Container.abouts.length)];
         return aboutMe;
     }
 
@@ -115,6 +196,7 @@ public class Profile {
      * @return the activities
      */
     public String getActivities() {
+        activities = Container.activites[getRandom(Container.activites.length)];
         return activities;
     }
 
@@ -122,6 +204,16 @@ public class Profile {
      * @return the birthday
      */
     public Date getBirthday() {
+        GregorianCalendar gc = new GregorianCalendar();
+        Integer year = randBetween(1930, 2000);
+        gc.set(Calendar.YEAR, year);
+
+        Integer dayOfYear = randBetween(1,
+                gc.getActualMaximum(Calendar.DAY_OF_YEAR));
+        gc.set(Calendar.DAY_OF_YEAR, dayOfYear);
+
+        birthday = new Date(gc.getTime().getTime());
+
         return birthday;
     }
 
@@ -143,6 +235,7 @@ public class Profile {
      * @return the favouriteBooks
      */
     public String getFavouriteBooks() {
+        favouriteBooks = Container.books[getRandom(Container.books.length)];
         return favouriteBooks;
     }
 
@@ -150,6 +243,7 @@ public class Profile {
      * @return the favouriteMovies
      */
     public String getFavouriteMovies() {
+        favouriteMovies = Container.movies[getRandom(Container.movies.length)];
         return favouriteMovies;
     }
 
@@ -157,6 +251,7 @@ public class Profile {
      * @return the favouriteMusic
      */
     public String getFavouriteMusic() {
+        favouriteMusic = Container.music[getRandom(Container.music.length)];
         return favouriteMusic;
     }
 
@@ -164,6 +259,7 @@ public class Profile {
      * @return the favouriteQuotes
      */
     public String getFavouriteQuotes() {
+        favouriteQuotes = Container.quotes[getRandom(Container.quotes.length)];
         return favouriteQuotes;
     }
 
@@ -171,6 +267,7 @@ public class Profile {
      * @return the favouriteTVShows
      */
     public String getFavouriteTVShows() {
+        favouriteTVShows = Container.movies[getRandom(Container.movies.length)];
         return favouriteTVShows;
     }
 
@@ -193,6 +290,7 @@ public class Profile {
      * @return the interests
      */
     public String getInterests() {
+        interests = Container.activites[getRandom(Container.activites.length)];
         return interests;
     }
 
@@ -205,9 +303,23 @@ public class Profile {
     }
 
     /**
-     * @return the pictureURL
+     * @return the maxInserts
      */
-    public String getPictureURL() {
+    public Integer getMaxInserts() {
+        return maxInserts;
+    }
+
+    /**
+     * @return the pictureURL
+     * @throws NoSuchAlgorithmException
+     * @throws UnsupportedEncodingException
+     */
+    public String getPictureURL() throws NoSuchAlgorithmException,
+            UnsupportedEncodingException {
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        byte[] bytesOfMessage = getUserId().toString().getBytes("UTF-8");
+        pictureURL = "http://socialpage.com/"
+                + messageDigest.digest(bytesOfMessage).toString();
         return pictureURL;
     }
 
@@ -215,6 +327,7 @@ public class Profile {
      * @return the politicanViews
      */
     public String getPoliticanViews() {
+        politicanViews = Container.politicanViews[getRandom(Container.politicanViews.length)];
         return politicanViews;
     }
 
@@ -229,6 +342,7 @@ public class Profile {
      * @return the religion
      */
     public String getReligion() {
+        religion = Container.religions[getRandom(Container.religions.length)];
         return religion;
     }
 
@@ -236,13 +350,16 @@ public class Profile {
      * @return the significantOtherId
      */
     public Integer getSignificantOtherId() {
+        significantOtherId = randBetween(1, getUserId() - 1);
         return significantOtherId;
     }
 
     /**
      * @return the updateTime
      */
-    public Timestamp getUpdateTime() {
+    public Time getUpdateTime() {
+        java.util.Date date = new java.util.Date();
+        updateTime = new Time(date.getTime());
         return updateTime;
     }
 
@@ -250,7 +367,58 @@ public class Profile {
      * @return the userId
      */
     public Integer getUserId() {
-        return userId;
+        return userId++;
+    }
+
+    /**
+     * This method starts process for insert random data to database.
+     */
+    public void insertRandomData() {
+        logger.info("Process for insert data is running");
+        for (int i = 0; i < getMaxInserts(); i++) {
+            try {
+                MariaDBConnection
+                        .executeStatement(createInsert(MariaDBConnection
+                                .getConn()));
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        logger.info("End of process");
+    }
+
+    /**
+     * This method creates data between given range.
+     * 
+     * @param start
+     * @param end
+     * @return Value between given range.
+     */
+    public Integer randBetween(int start, int end) {
+        logger.warning(start + " " + end + " "
+                + (start + (int) Math.round(Math.random() * (end - start))));
+        return start + (int) Math.round(Math.random() * (end - start));
+    }
+
+    /**
+     * @param maxInserts
+     *            the maxInserts to set
+     */
+    public void setMaxInserts(Integer maxInserts) {
+        this.maxInserts = maxInserts;
+    }
+
+    /**
+     * This method is only for test purpose.
+     * 
+     * @return something expected.
+     */
+    public Date testMethod() {
+        return getBirthday();
     }
 
 }
