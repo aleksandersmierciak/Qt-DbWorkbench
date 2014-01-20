085c594177155ad1681bf65496b18ce016c01ed5
m4gik <michal.szczygiel@wp.pl>
2014-01-20 21:12:33 +0100

    Added class for getting random enum value

---
+++ MariaDB/MariaDB Java WorkBench/databaseinsertion/src/main/java/com/m4gik/util/RandomEnum.java
@@ -0,0 +1,24 @@
+package com.m4gik.util;
+
+import java.util.Random;
+
+/**
+ * This class is for perform genreating enums.
+ * 
+ * @author Michał Szczygieł <michal.szczygiel@wp.pl>
+ * 
+ */
+class RandomEnum<E extends Enum<?>> {
+
+    private static final Random RND = new Random();
+
+    private final E[] values;
+
+    public RandomEnum(Class<E> token) {
+        values = token.getEnumConstants();
+    }
+
+    public E random() {
+        return values[RND.nextInt(values.length)];
+    }
+}
