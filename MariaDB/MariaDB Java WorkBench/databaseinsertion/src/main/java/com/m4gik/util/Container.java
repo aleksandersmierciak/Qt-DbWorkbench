a209ba625b3e8b4d5926a5412d26d890054d40f0
m4gik <michal.szczygiel@wp.pl>
2014-01-20 20:11:20 +0100

    Update the Container with new  example data

--- MariaDB/MariaDB Java WorkBench/databaseinsertion/src/main/java/com/m4gik/util/Container.java
+++ MariaDB/MariaDB Java WorkBench/databaseinsertion/src/main/java/com/m4gik/util/Container.java
@@ -9,25 +9,98 @@ package com.m4gik.util;
 public class Container {
 
     /**
+     * Sample abouts.
+     */
+    public static final String abouts[] = {
+            "You might not think it at first glance, but my favourite music is of the heavier variety…you’re more likely to find me on the edge of a mosh pit than dancing in a club.",
+            " …but I also have an embarrassing obsession with Justin Bieber. And I also can’t believe I just put that on the internet.",
+            " I got my first tattoo last year – a pair of glasses on my inner bicep.",
+            "I have the WORST diet. I don’t eat any fruit and vegetables – to the point that I feel uncomfortable sitting near people eating certain fruits. Yeah, I’m weird.",
+            "I used to dance a lot as a young’un, and quit ballet when I was around 17…and started this blog not long after!",
+            " I also used to be a majorette (y’know the ones..batons, pom poms and lots of glitter!)",
+            "My favourite ever meal would probably have to be a kebab (+ chips and cheese). “What about the burgers?!” I hear you cry! Well, I love them too…kebabs just don’t have quite as good a reputation, do they?"
+
+    };
+
+    /**
+     * Sample activites.
+     */
+    public static final String activites[] = { "football", "cycling",
+            "cracking", "P.E", "running" };
+
+    /**
+     * Sample books.
+     */
+    public static final String books[] = { "The Hunger Games", "The Host",
+            "The Help", "Dead Until Dark", "Mockingjay", "Lord of the Flies",
+            "Lord of the Flies", "Because of Winn-Dixie",
+            "James Potter and the Hall of Elders' Crossing", "Divergent",
+            "I Am Number Four", "Game of Thron" };
+
+    /**
      * Sample cities.
      */
     public static final String cities[] = { "Kraków", "Amsterdam", "Berlin",
             "Helsinki", "Hong Kong", "London", "Luxemburg", "New York", "Oslo",
             "Paris", "Rome", "Stockholm", "Tokyo", "Turku" };
-
     /**
      * Sample first names.
      */
     public static final String[] firstNames = { "Peter", "Alice", "Joshua",
             "Mike", "Olivia", "Nina", "Alex", "Rita", "Dan", "Umberto",
             "Henrik", "Rene", "Lisa", "Marge", "Michał" };
-
     /**
      * Sample last names.
      */
     public static final String[] lastNames = { "Smith", "Gordon", "Simpson",
             "Brown", "Clavel", "Simons", "Verne", "Scott", "Allison", "Gates",
             "Rowling", "Barks", "Ross", "Schneider", "Tate", "Szczygieł" };
+
+    /**
+     * Sample movies.
+     */
+    public static final String movies[] = { "Planet 51", "The Maltese Falcon",
+            "Kalifornia", "Once Upon a Time in Mexico",
+            "Robinson Crusoe on Mars", "Double Jeopardy", "Megamind",
+            "Pride & Prejudice", "Walking Tall", "Hugo", "Admiral",
+            "The Dead Zone", "Character", "Blazing Saddles", "The Corporation",
+            "Lady Vengeance" };
+
+    /**
+     * Sample music.
+     */
+    public static final String[] music = { "Nirvana - In Bloom",
+            "Cat Stevens - If you want to sing out", "Your Song - Elton John",
+            "ATC - All Around The World (la la la la la la la la)",
+            "Goo Goo Dolls - Slide", "Nirvana - Come As You Are",
+            "Adele - Rolling in the Deep", "Linkin Park- Crawling",
+            "Muse - Uprising", "Bob Marley- Three Little Birds",
+            "Kansas - Dust In The Wind", "Oasis - Wonderwall" };
+
+    /**
+     * Sample politician views.
+     */
+    public static final String politicanViews[] = { "Anarchism", "Communism",
+            "Conservatism", "Environmentalism", "Feminism", "Liberalism",
+            "Nationalism", "Religious ideologies", "Socialism", "Marxism" };
+
+    /**
+     * Sample quotations.
+     */
+    public static final String quotes[] = {
+            "The test of a first-rate intelligence is the ability to hold two opposed ideas in the mind at the same time, and still retain the ability to function.    F. Scott Fitzgerald (1896 - 1940)",
+            " Politicians are the same all over. They promise to build a bridge even where there is no river. Nikita Khrushchev",
+            "Let us make one point, that we meet each other with a smile, when it is difficult to smile. Smile at each other, make time for each other in your family. Mother Teresa",
+            "Psychiatry enables us to correct our faults by confessing our parents' shortcomings. Laurence J. Peter",
+            "If a man is offered a fact which goes against his instincts, he will scrutinize it closely, and unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will accept it even on the slightest evidence. The origin of myths is explained in this way. Bertrand Russell ",
+            "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former. Albert Einstein" };
+
+    /**
+     * Sample religions.
+     */
+    public static final String religions[] = { "General", "Buddhism",
+            "Christianity", "Hinduism", "Islam", "Judaism", "Mormonism",
+            "Sikhism" };
 
     /**
      * Sample streets.
